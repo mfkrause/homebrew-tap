2b3f858fff9a326e4811b1ec10f80a0e111387d2
Maximilian Krause <max@kuatsu.de>
2026-09-02 11:28:05 +0200

    chore: mark Consul cask as auto-updating

--- Casks/consul.rb
+++ Casks/consul.rb
@@ -3,18 +3,20 @@ cask "consul" do
 
   on_arm do
     sha256 "b3fb635984f2745d5459a1e410161fcc923c798bc49ce6cb351abad0a7684d3c"
+
     url "https://getconsul.app/releases/Consul-#{version}-arm64.dmg"
   end
-
   on_intel do
     sha256 "8bf1bb3697f26e2af7154ae20d60d554db0c91519a690db091570ed93dbe28da"
+
     url "https://getconsul.app/releases/Consul-#{version}-x86_64.dmg"
   end
 
   name "Consul"
   desc "Automatically converts files when their extension is changed"
-  homepage "https://getconsul.app"
+  homepage "https://getconsul.app/"
 
+  auto_updates true
   depends_on macos: :sonoma
 
   app "Consul.app"
